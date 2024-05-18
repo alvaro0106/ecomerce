4e061ef38a409e5a6054c2208ed089d943628c25
Alvaro López Villanueva <43691672+alvaro0106@users.noreply.github.com>
2024-05-18 07:25:48 -0600

    inserta mapa2

--- web/views/modules/footer.php
+++ web/views/modules/footer.php
@@ -1,3 +1,135 @@
+<style>
+    a:link, a:visited, a:hover, a:active{
+      text-decoration: none !important;
+      color: inherit !important;
+    }
+</style>
+
+<div class="containner-fluid bg-dark small">
+
+    <div class="container py-5 text-light">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3"> 
+            <div class="col row"> 
+
+              <!-- Categoria 1 -->
+              <div class="col-12 col-sm-4 col-md-3 col-lg-4">
+                  <h4 class="lead">
+                    <a href="#" class="text-uppercase">Ropa</a>
+                  </h4>
+                    <hr class="border-white">
+                    <ul>
+                      <li>
+                        <a href="#">Ropa para Dama</a>
+                      </li>
+                      <li>
+                        <a href="#">Ropa Caballero</a>
+                      </li>
+                      <li>
+                        <a href="#">Ropa Deportiva</a>
+                      </li>
+                      <li>
+                        <a href="#">Ropa para menor</a>
+                      </li>
+
+                    </ul>
+              </div>
+              
+              <!-- Categoria 2 -->
+              <div class="col-12 col-sm-4 col-md-3 col-lg-4">
+                  <h4 class="lead">
+                    <a href="#" class="text-uppercase">calzado</a>
+                  </h4>
+                    <hr class="border-white">
+                    <ul>
+                      <li>
+                        <a href="#">Calzado para Dama</a>
+                      </li>
+                      <li>
+                        <a href="#">Calzado Caballero</a>
+                      </li>
+                      <li>
+                        <a href="#">Calzado Deportiva</a>
+                      </li>
+                      <li>
+                        <a href="#">Calzado para menor</a>
+                      </li>
+
+                    </ul>
+              </div>
+
+              <!-- Categoria 3 -->
+              <div class="col-12 col-sm-4 col-md-3 col-lg-4">
+                  <h4 class="lead">
+                    <a href="#" class="text-uppercase">Tecnologia</a>
+                  </h4>
+                    <hr class="border-white">
+                    <ul>
+                      <li>
+                        <a href="#">Computadoras</a>
+                      </li>
+                      <li>
+                        <a href="#">Pantallas</a>
+                      </li>
+                      <li>
+                        <a href="#">Servidores</a>
+                      </li>
+                      <li>
+                        <a href="#">Celulares</a>
+                      </li>
+
+                    </ul>
+              </div>
+
+              <!-- Categoria 4 -->
+              <div class="col-12 col-sm-4 col-md-3 col-lg-4">
+                  <h4 class="lead">
+                    <a href="#" class="text-uppercase">Cursos</a>
+                  </h4>
+                    <hr class="border-white">
+                    <ul>
+                      <li>
+                        <a href="#">Programacion</a>
+                      </li>
+                      <li>
+                        <a href="#">Idiomas</a>
+                      </li>
+                      <li>
+                        <a href="#">Desarrolo</a>
+                      </li>
+                      <li>
+                        <a href="#">Testeo</a>
+                      </li>
+
+                    </ul>
+              </div>
+
+            </div>
+            <div class="col my-3 my-lg-0 px-lg-5 text-light">
+              <h1 class="lead small">Dudas e inquietudes, contactenos en: </h1>
+              <br>
+              <h1 class="lead small  ">
+                  <i class="fa fa-phone-square pe-2"></i>(55) 555-555-555
+                  <br><br>
+                  <i class="fa fa-envelope pe-2"></i>coreofalso@gmail.com
+                  <br><br>
+                  <i class="fa fa-map-marker pe-2"></i>Calle falsa #444 col. No existe
+                  <br><br>
+                  Leon Guanajuato | Mexico
+              </h1>
+
+              <iframe class="mt-3" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3722.088559671407!2d-101.6584032!3d21.109035199999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842bbe52f6155555%3A0x507f5e9bd38e531f!2sExplora!5e0!3m2!1ses-419!2smx!4v1716007935858!5m2!1ses-419!2smx" 
+                width="100%" height="200" frameborder="0" style="border:0;" allowfullscreen="">
+              </iframe>
+
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+
 <!-- Main Footer -->
 <footer class="main-footer">
     <!-- To the right -->
--- web/views/modules/natvbar.php
+++ web/views/modules/natvbar.php
@@ -1,226 +1,140 @@
-  <!-- Navbar -->
+<!-- Navbar -->
 
-  <Style>
-    .dropdown-toggle::after { 
-      display: none !important;
-    }
+<Style>
+  .dropdown-toggle::after { 
+    display: none !important;
+  }
 
-    ul{
-      padding: 0;
-      list-style: none;
-      text-decoration: none;
-    }
-  </Style>
-
+  ul {
+    padding: 0;
+    list-style: none;
+    text-decoration: none;
+  }
+</Style>
 
 <div class="container">
   <div class="row">
 
     <div class="col-12 col-lg-2 mt-1">
-
-        <div class="d-flex justify-content-center">
-
+      <div class="d-flex justify-content-center">
         <a href="<?php echo $path ?>views/assets/js/index3.html" class="navbar-brand">
-          <img src="<?php echo $path ?>views/assets/img/adminlte/AdminLTELogo.png" class="brand-image imp-fluid py-3 py-5 py-lg-0 pe-lg-3">
+          <img src="<?php echo $path ?>views/assets/img/adminlte/DULCECANELA-min2.png" class="brand-image imp-fluid py-3 py-5 py-lg-0 pe-lg-3">
         </a>
-        </div>
-
+      </div>
     </div>
 
-    <div class="col-12 col-lg-7 col-xl-8 mt-1 px-3 px-lg-0">
-
+    <div class="col-12 col-lg-7 col-xl-8 mt-2 px-3 px-lg-0">
       <a class="nav-link float-start" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
 
-      <div class="dropdown px-1 float-start" style="background:#47BAC1; color:whithe">
-            
-          <a id="dropdownSubMenu1" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle text-uppercase">Categorias <i class="ps-2 fas fa-th-list"></i> </a>
+      <div class="dropdown px-1 float-start" style="background:#47BAC1; color:white">
+        <a id="dropdownSubMenu1" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle text-uppercase">Categorias <i class="ps-2 fas fa-th-list"></i> </a>
 
-            <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-              
-            <!-- Level two dropdown-->
-            <li class="dropdown-submenu dropdown-hover">
-                  <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
-                    <i class="fas fa-tshirt pe-2 fa-xs"></i> ropa
-                  </a>
-                  <ul  class="border-0 shadow py-3 ps-3 d-block d-lg-none">
-                    <li>
-                      <a tabindex="-1" href="#" class="dropdown-item text-upercase">Calzado para dama</a>
-                    </li>
-                    <li>
-                      <a tabindex="-1" href="#" class="dropdown-item text-upercase">Calzado para hombre</a>
-                    </li>
-                    <li>
-                      <a tabindex="-1" href="#" class="dropdown-item text-upercase">Calzado deportivos</a>
-                    </li>
-                    <li>
-                      <a tabindex="-1" href="#" class="dropdown-item text-upercase">Calzado para infantil</a>
-                    </li>
-                  </ul>
+        <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+          <!-- Level two dropdown-->
+          <li class="dropdown-submenu dropdown-hover">
+            <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
+              <i class="fas fa-tshirt pe-2 fa-xs"></i> ropa
+            </a>
+            <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none">
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Calzado para dama</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Calzado para hombre</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Calzado deportivos</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Calzado para infantil</a></li>
+            </ul>
+            <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Ropa para dama</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Ropa para hombre</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Ropa deportiva</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Ropa para infantil</a></li>
+            </ul>
+          </li>
+          <!-- End level two -->
 
+          <!-- Level three dropdown-->
+          <li class="dropdown-submenu dropdown-hover">
+            <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
+              <i class="fas fa-shoe-prints pe-2 fa-xs"></i> calzado
+            </a>
+            <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none">
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Calzado para dama</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Calzado para hombre</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Calzado deportivos</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Calzado para infantil</a></li>
+            </ul>
+            <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Calzado para dama</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Calzado para hombre</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Calzado deportivos</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Calzado para infantil</a></li>
+            </ul>
+          </li>
+          <!-- End level three -->
 
-                  <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
-                    <li>
-                      <a tabindex="-1" href="#" class="dropdown-item text-upercase">Ropa para dama</a>
-                    </li>
-                    <li>
-                      <a tabindex="-1" href="#" class="dropdown-item text-upercase">Ropa para hombre</a>
-                    </li>
-                    <li>
-                      <a tabindex="-1" href="#" class="dropdown-item text-upercase">Ropa para deportiva</a>
-                    </li>
-                    <li>
-                      <a tabindex="-1" href="#" class="dropdown-item text-upercase">Ropa para infantil</a>
-                    </li>
-                  </ul>
-              </li>
-            <!-- End level two -->
-
-            <!-- Level tree dropdown-->
-            <li class="dropdown-submenu dropdown-hover">
-                <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
-                  <i class="fas fa-shoe-prints pe-2 fa-xs"></i> calzado
-                </a>
-
-                  <ul  class="border-0 shadow py-3 ps-3 d-block d-lg-none">
-                    <li>
-                      <a tabindex="-1" href="#" class="dropdown-item text-upercase">Calzado para dama</a>
-                    </li>
-                    <li>
-                      <a tabindex="-1" href="#" class="dropdown-item text-upercase">Calzado para hombre</a>
-                    </li>
-                    <li>
-                      <a tabindex="-1" href="#" class="dropdown-item text-upercase">Calzado deportivos</a>
-                    </li>
-                    <li>
-                      <a tabindex="-1" href="#" class="dropdown-item text-upercase">Calzado para infantil</a>
-                    </li>
-                  </ul>
-
-
-                <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
-                  <li>
-                    <a tabindex="-1" href="#" class="dropdown-item text-upercase">Calzado para dama</a>
-                  </li>
-                  <li>
-                    <a tabindex="-1" href="#" class="dropdown-item text-upercase">Calzado para hombre</a>
-                  </li>
-                  <li>
-                    <a tabindex="-1" href="#" class="dropdown-item text-upercase">Calzado deportivos</a>
-                  </li>
-                  <li>
-                    <a tabindex="-1" href="#" class="dropdown-item text-upercase">Calzado para infantil</a>
-                  </li>
-                </ul>
-            </li>
-          <!-- End level tree -->
-
-          <!-- Level Tecnologia dropdown-->
-        <li class="dropdown-submenu dropdown-hover">
+          <!-- Level tecnologia dropdown-->
+          <li class="dropdown-submenu dropdown-hover">
             <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
               <i class="fas fa-laptop pe-2 fa-xs"></i> tecnologia
             </a>
-
-            <ul  class="border-0 shadow py-3 ps-3 d-block d-lg-none">
-              <li>
-                <a tabindex="-1" href="#" class="dropdown-item text-upercase">Computacion</a>
-              </li>
-              <li>
-                <a tabindex="-1" href="#" class="dropdown-item text-upercase">Electricidad</a>
-              </li>
-              <li>
-                <a tabindex="-1" href="#" class="dropdown-item text-upercase">belleza</a>
-              </li>
-              <li>
-                <a tabindex="-1" href="#" class="dropdown-item text-upercase">programacion</a>
-              </li>
+            <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none">
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Computacion</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Electricidad</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Belleza</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Programacion</a></li>
             </ul>
-
             <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
-              <li>
-                <a tabindex="-1" href="#" class="dropdown-item text-upercase">Computacion</a>
-              </li>
-              <li>
-                <a tabindex="-1" href="#" class="dropdown-item text-upercase">Electricidad</a>
-              </li>
-              <li>
-                <a tabindex="-1" href="#" class="dropdown-item text-upercase">belleza</a>
-              </li>
-              <li>
-                <a tabindex="-1" href="#" class="dropdown-item text-upercase">programacion</a>
-              </li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Computacion</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Electricidad</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Belleza</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Programacion</a></li>
             </ul>
-        </li>
-      <!-- End level Tecnologia -->
+          </li>
+          <!-- End level tecnologia -->
 
-      <!-- Level Tecnologia dropdown-->
-      <li class="dropdown-submenu dropdown-hover">
-          <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
-            <i class="fas fa-graduation-cap pe-2 fa-xs"></i> Cursos
-          </a>
-
-
-          <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none">
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item text-upercase">Computacion</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item text-upercase">Mecanica</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item text-upercase">Pintura</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item text-upercase">cocina</a>
-            </li>
-          </ul>
-
-
-          <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item text-upercase">Computacion</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item text-upercase">Mecanica</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item text-upercase">Pintura</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item text-upercase">cocina</a>
-            </li>
-          </ul>
-      </li>
-    <!-- End level Tecnologia -->
-
-  </div>
-
-
-  <form class="form-inline ">
-          <div class="input-group input-group w-100 me-lg-4">
-            <input class="form-control rouded-0 p-3 pe-5" type="search" placeholder="buscar..." style="height: 40px;">
-            <div class="input-group-append px-2" style="background:#47BAC1; color:whithe">
-              <button class="btn btn-navbar text-white" type="submit">
-                <i class="fas fa-search"></i>
-              </button>
-            </div>
-          </div>
-  </form>
-
-  <div class="col-12 col-lg-3 col-xl-2 mt-1 px-3 px-lg-0">
-
-    <div class="my-2 my-lg- d-flex justify-content-center">
-      <a href="#">
-        <button class="bt btn-default float-start rounded-0 border-0 py-2 px-3" style="background:#47BAC1; color:whithe">
-        <i class="fa fa-shopping-cart"></i>
-      </button>
-      </a>
-
-      <div class="small border float-atart ps-2 pe-5 w-100">
-        
-          TU CESTA <span>0</span>USD $ <samp>0</samp>
-
+          <!-- Level cursos dropdown-->
+          <li class="dropdown-submenu dropdown-hover">
+            <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
+              <i class="fas fa-graduation-cap pe-2 fa-xs"></i> cursos
+            </a>
+            <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none">
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Computacion</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Mecanica</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Pintura</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Cocina</a></li>
+            </ul>
+            <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Computacion</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Mecanica</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Pintura</a></li>
+              <li><a tabindex="-1" href="#" class="dropdown-item text-uppercase">Cocina</a></li>
+            </ul>
+          </li>
+          <!-- End level cursos -->
+        </ul>
       </div>
-      
+
+      <form class="form-inline">
+        <div class="input-group input-group w-100 me-lg-4">
+          <input class="form-control rounded-0 p-3 pe-5" type="search" placeholder="buscar..." style="height: 40px;">
+          <div class="input-group-append px-2" style="background:#47BAC1; color:white">
+            <button class="btn btn-navbar text-white" type="submit">
+              <i class="fas fa-search"></i>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+
+    <div class="col-12 col-lg-3 col-xl-2 mt-0 px-3 px-lg-0">
+      <div class="my-2 my-lg- d-flex justify-content-center">
+        <a href="#">
+          <button class="bt btn-default float-start rounded-0 border-0 py-2 px-3" style="background:#47BAC1; color:white">
+            <i class="fa fa-shopping-cart"></i>
+          </button>
+        </a>
+        <div class="small border float-start ps-2 pe-5 w-100">
+          TU CESTA <span>0</span> <br> USD $ <samp>0</samp>
+        </div>
+      </div>
     </div>
 
   </div>
